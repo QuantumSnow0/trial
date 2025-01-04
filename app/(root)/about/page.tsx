8680bad45cc33168c9bafec180e1b9d97fc13ed1
bonface <bmuthuri93@gmail.com>
2025-01-04 19:39:30 +0300

    Update [description of update]

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,7 +1,6 @@
 import Fact from "@/components/Fact";
 import React, { Suspense } from "react";
 import next from "@/public/next-button.png";
-export const experimental_ppr = true;
 
 import { SkeletonDemo } from "@/components/SkeletonDemo";
 import Button from "@/components/Button";
@@ -12,7 +11,7 @@ const page = () => {
     <section className="flex flex-1 flex-col w-full min-h-screen items-center justify-center">
       <div className="flex flex-col justify-between">
         <h1 className="text-3xl  text-center underline mb-1">
-          Random Facts about cats
+          Random Facts about Cats
         </h1>
         <Suspense fallback={<SkeletonDemo />}>
           <Fact />
